e4a0e0ced6256aaf8a58f98aec40be1e04a3d000
Alexey Polyakov <folsten@alfa.im>
2024-01-09 12:01:03 +0300

    changed header exit text

--- src/components/TheHeader/TheHeader.vue
+++ src/components/TheHeader/TheHeader.vue
@@ -3,7 +3,7 @@
     <div @click="isMenuActive=!isMenuActive" :class="{'bg-gray-2c2d2f':isMenuActive}" class="relative flex bg- h-full pl-[30px] pr-[30px] justify-center items-center border-r border-gray-dcdcdc cursor-pointer">
       <SvgIcon v-if="!isMenuActive" name="burger-menu" class="w-[30px] h-[30px]"/>
       <SvgIcon v-if="isMenuActive" name="cross" class="w-[30px] h-[30px]"/>
-      <div v-if="isMenuActive" class="absolute pl-[30px] text-white w-[276px] flex items-center bg-gray-2c2d2f -bottom-[80px] left-0 h-[80px]">wefwef</div>
+      <div v-if="isMenuActive" class="absolute pl-[30px] text-white w-[276px] flex items-center bg-gray-2c2d2f -bottom-[80px] left-0 h-[80px]">Выход</div>
     </div>
     <div class="flex justify-between items-center w-full">
       <div class="flex items-center ml-7">
